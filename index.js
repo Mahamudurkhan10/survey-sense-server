c003921226c9e49a11d5fd281ca85268cd37c745
Mahamudurkhan10 <mahamudurkhan10@gmail.com>
2024-06-10 07:23:18 +0600

    code sending

--- index.js
+++ index.js
@@ -188,6 +188,13 @@ async function run() {
       res.send(result)
      
     })
+    app.get('/responseMail/:email',  async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const result = await responseCollection.find(query).toArray();
+      res.send(result)
+     
+    })
     app.get('/responseOne/:id',  async (req, res) => {
       const id = req.params.id;
       const query = { resId: id };
